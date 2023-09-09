a0a841e1c920da5ff25a9572e9ced68110d34ffe
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-09-09 17:46:09 -0300

    projeto concluído

--- index.php
+++ index.php
@@ -8,16 +8,94 @@
 </head>
 <body>
     <div class="box1">
-        box 1
+        <div class="content-title">
+            <span class="title">Passeio de Balões</span>
+        </div>
     </div>
     <div class="content">
         <h3>Parallax</h3>
         <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum recusandae inventore placeat iusto aspernatur facilis, consectetur aliquid voluptas vel doloribus, asperiores perferendis at ut eum? Ut iusto eum repudiandae sunt?
         </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque repudiandae tenetur iste voluptas fugiat eius eum dolorum recusandae alias odio soluta, magni animi necessitatibus possimus laudantium quod placeat molestias doloribus?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque perspiciatis, modi distinctio incidunt autem ullam, quo rerum ipsa fugit est, quas voluptatem itaque! Quibusdam, consectetur cumque possimus doloremque dolorum eligendi?
+        </p>
     </div>
     <div class="box2">
-        box 2
+    <div class="content-title">
+            <span class="title">Pegando a Estrada</span>
+        </div>
+    </div>
+    <div class="content">
+        <h3>Parallax</h3>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum recusandae inventore placeat iusto aspernatur facilis, consectetur aliquid voluptas vel doloribus, asperiores perferendis at ut eum? Ut iusto eum repudiandae sunt?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque repudiandae tenetur iste voluptas fugiat eius eum dolorum recusandae alias odio soluta, magni animi necessitatibus possimus laudantium quod placeat molestias doloribus?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque perspiciatis, modi distinctio incidunt autem ullam, quo rerum ipsa fugit est, quas voluptatem itaque! Quibusdam, consectetur cumque possimus doloremque dolorum eligendi?
+        </p>
+    </div>
+    <div class="box3">
+        <div class="content-title">
+            <span class="title">Por do Sol</span>
+        </div>
+    </div>
+    <div class="content">
+        <h3>Parallax</h3>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum recusandae inventore placeat iusto aspernatur facilis, consectetur aliquid voluptas vel doloribus, asperiores perferendis at ut eum? Ut iusto eum repudiandae sunt?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque repudiandae tenetur iste voluptas fugiat eius eum dolorum recusandae alias odio soluta, magni animi necessitatibus possimus laudantium quod placeat molestias doloribus?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque perspiciatis, modi distinctio incidunt autem ullam, quo rerum ipsa fugit est, quas voluptatem itaque! Quibusdam, consectetur cumque possimus doloremque dolorum eligendi?
+        </p>
+    </div>
+    <div class="box4">
+        <div class="content-title">
+            <span class="title">Brisa das Montanhas</span>
+        </div>
+    </div>
+    <div class="content">
+        <h3>Parallax</h3>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum recusandae inventore placeat iusto aspernatur facilis, consectetur aliquid voluptas vel doloribus, asperiores perferendis at ut eum? Ut iusto eum repudiandae sunt?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque repudiandae tenetur iste voluptas fugiat eius eum dolorum recusandae alias odio soluta, magni animi necessitatibus possimus laudantium quod placeat molestias doloribus?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque perspiciatis, modi distinctio incidunt autem ullam, quo rerum ipsa fugit est, quas voluptatem itaque! Quibusdam, consectetur cumque possimus doloremque dolorum eligendi?
+        </p>
+    </div>
+    <div class="box5">
+        <div class="content-title">
+            <span class="title">Tranquilidade do Mar</span>
+        </div>
+    </div>
+    <div class="content">
+        <h3>Parallax</h3>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum recusandae inventore placeat iusto aspernatur facilis, consectetur aliquid voluptas vel doloribus, asperiores perferendis at ut eum? Ut iusto eum repudiandae sunt?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque repudiandae tenetur iste voluptas fugiat eius eum dolorum recusandae alias odio soluta, magni animi necessitatibus possimus laudantium quod placeat molestias doloribus?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque perspiciatis, modi distinctio incidunt autem ullam, quo rerum ipsa fugit est, quas voluptatem itaque! Quibusdam, consectetur cumque possimus doloremque dolorum eligendi?
+        </p>
+    </div>
+    <div class="box6">
+        <div class="content-title">
+            <span class="title">A Brisa do Mar</span>
+        </div>
     </div>
 </body>
 </html>
