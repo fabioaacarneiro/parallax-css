742d3513062cb71c7fb84745f3c82de654f0bb04
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-09-09 17:10:01 -0300

    projeto base criado

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Parallax</title>
+</head>
+<body>
+    <div class="box1">
+        box 1
+    </div>
+    <div class="content">
+        <h3>Parallax</h3>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum recusandae inventore placeat iusto aspernatur facilis, consectetur aliquid voluptas vel doloribus, asperiores perferendis at ut eum? Ut iusto eum repudiandae sunt?
+        </p>
+    </div>
+    <div class="box2">
+        box 2
+    </div>
+</body>
+</html>
